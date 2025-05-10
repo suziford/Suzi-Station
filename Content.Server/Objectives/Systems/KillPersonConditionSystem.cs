58e8ea242538e26b968fd5da5bb8e0d804e32d55
Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
2025-05-10 19:09:36 +0500

    Correct kill objectives (#214)
    * kill objectives only target people with jobs (#2799)
    
    * implement OnlyChoosableJobs for PickRandomPerson
    
    * add onlyChoosableJobs to kill and teach lesson objectives
    
    * fix the port
    
    * :trollface:
    
    * what
    
    * :trollface:
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>
    
    * minor fixes
    
    ---------
    
    Co-authored-by: deltanedas <39013340+deltanedas@users.noreply.github.com>

--- Content.Server/Objectives/Systems/KillPersonConditionSystem.cs
+++ Content.Server/Objectives/Systems/KillPersonConditionSystem.cs
@@ -17,12 +17,18 @@
 using Content.Server.GameTicking.Rules;
 using Content.Server._Goobstation.Objectives.Components;
 using Content.Server.Objectives.Components;
+using Content.Server.Revolutionary.Components; //Reserve
 using Content.Server.Shuttles.Systems;
 using Content.Shared.CCVar;
 using Content.Shared.Roles.Jobs;
 using Content.Shared.Mind;
 using Content.Shared.Objectives.Components;
+using Content.Shared.Roles; // DeltaV
+using Content.Shared.Roles.Jobs; // DeltaV
 using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes; // DeltaV
+using Robust.Shared.Random;
+using System.Linq;
 
 namespace Content.Server.Objectives.Systems;
 
@@ -35,6 +41,9 @@ public sealed class KillPersonConditionSystem : EntitySystem
     [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly SharedJobSystem _job = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!; // DeltaV
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedRoleSystem _role = default!; // DeltaV
     [Dependency] private readonly TargetObjectiveSystem _target = default!;
     [Dependency] private readonly TraitorRuleSystem _traitor = default!;
 
@@ -51,6 +60,79 @@ public sealed class KillPersonConditionSystem : EntitySystem
             return;
 
         args.Progress = GetProgress(target.Value, comp.RequireDead);
+    }
+
+    private void OnPersonAssigned(Entity<PickRandomPersonComponent> ent, ref ObjectiveAssignedEvent args)
+    {
+        AssignRandomTarget(ent, ref args, _ => true, ent.Comp.OnlyChoosableJobs); // DeltaV: pass onlyJobs
+    }
+
+    private void OnHeadAssigned(Entity<PickRandomHeadComponent> ent, ref ObjectiveAssignedEvent args)
+    {
+        AssignRandomTarget(ent, ref args, mindId =>
+            TryComp<MindComponent>(mindId, out var mind) &&
+            mind.OwnedEntity is { } ownedEnt &&
+            HasComp<CommandStaffComponent>(ownedEnt));
+    }
+
+    // DeltaV: added onlyJobs
+    private void AssignRandomTarget(EntityUid uid, ref ObjectiveAssignedEvent args, Predicate<EntityUid> filter, bool onlyJobs = true, bool fallbackToAny = true)
+    {
+        // invalid prototype
+        if (!TryComp<TargetObjectiveComponent>(uid, out var target))
+        {
+            args.Cancelled = true;
+            return;
+        }
+
+        // target already assigned
+        if (target.Target != null)
+            return;
+
+        // Get all alive humans, filter out any with TargetObjectiveImmuneComponent
+        var allHumans = _mind.GetAliveHumans(args.MindId)
+            .ToList();
+
+        // Begin DeltaV Additions: Only target people with jobs
+        if (onlyJobs)
+        {
+            allHumans.RemoveAll(mindId => !(
+                _role.MindHasRole<JobRoleComponent>((mindId.Owner, mindId.Comp), out var role) &&
+                role?.Comp1.JobPrototype is {} jobId &&
+                _proto.Index(jobId).SetPreference));
+        }
+        // End DeltaV Additions
+
+        // Can't have multiple objectives to kill the same person
+        foreach (var objective in args.Mind.Objectives)
+        {
+            if (HasComp<KillPersonConditionComponent>(objective) && TryComp<TargetObjectiveComponent>(objective, out var kill))
+            {
+                allHumans.RemoveAll(x => x.Owner == kill.Target);
+            }
+        }
+
+        // Filter out targets based on the filter
+        var filteredHumans = allHumans.Where(mind => filter(mind)).ToList();
+
+        // There's no humans and we can't fall back to any other target
+        if (filteredHumans.Count == 0 && !fallbackToAny)
+        {
+            args.Cancelled = true;
+            return;
+        }
+
+        // Pick between humans matching our filter or fall back to all humans alive
+        var selectedHumans = filteredHumans.Count > 0 ? filteredHumans : allHumans;
+
+        // Still no valid targets even after the fallback
+        if (selectedHumans.Count == 0)
+        {
+            args.Cancelled = true;
+            return;
+        }
+
+        _target.SetTarget(uid, _random.Pick(selectedHumans), target);
     }
 
     private float GetProgress(EntityUid target, bool requireDead)
