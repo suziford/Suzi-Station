305853d837dadde464fcedb420d8ed9029c2ec26
сачтик <51020966+ca4tuk@users.noreply.github.com>
2025-07-15 13:57:13 +0300

    Update ServerBanDef.cs

--- Content.Server/Database/ServerBanDef.cs
+++ Content.Server/Database/ServerBanDef.cs
@@ -116,7 +116,7 @@ namespace Content.Server.Database
 
             using (var client = new HttpClient())
             {
-                string apiUrl = "https://auth.spacestation14.com/api/query/userid?userid=" + userId;
+                string apiUrl = "https://auth.simplestation.org/api/query/userid?userid=" + userId;
 
                 HttpResponseMessage response = client.Send(new HttpRequestMessage(HttpMethod.Get, apiUrl));
 
