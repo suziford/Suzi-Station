d40a35d1f0ce994858872f7b645db49199caa0b7
echotry <xippl1@mail.ru>
2025-08-20 18:11:53 +0300

    Revert "Engine update / EE multiauth (#79)"
    This reverts commit 5c33a7d059d6d4f5bacb79906e42d77a23716810.

--- Content.Replay/Program.cs
+++ Content.Replay/Program.cs
@@ -1,9 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
-// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
 //
 // SPDX-License-Identifier: MIT
 
@@ -21,8 +17,8 @@ internal static class Program
             ContentModulePrefix = "Content.",
             ContentBuildDirectory = "Content.Replay",
             DefaultWindowTitle = "SS14 Replay",
-            UserDataDirectoryName = Environment.GetEnvironmentVariable("SS14_LAUNCHER_DATADIR") ?? "SimpleStation14", //EE multiauth
-            ConfigFileName = "replay.toml",
+            UserDataDirectoryName = "Space Station 14",
+            ConfigFileName = "replay.toml"
         });
     }
 }
