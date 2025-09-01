eb8ef9d5d95f5785336ec00f0d9b4e86b4e71566
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-01 01:56:52 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Reserve/EntityEffects/Effects/PlantMetabolism/PlantClearMutations.cs
+++ Content.Server/_Reserve/EntityEffects/Effects/PlantMetabolism/PlantClearMutations.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 sa1nt7331 <202271576+sa1nt7331@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Linq;
 using Content.Server.Botany;
 using Content.Server.Botany.Components;
