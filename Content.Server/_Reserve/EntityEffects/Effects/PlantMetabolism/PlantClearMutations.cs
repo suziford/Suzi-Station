8abf135b0c6305f290b4848c38dc2b93a5e42d70
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-03 19:07:30 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Reserve/EntityEffects/Effects/PlantMetabolism/PlantClearMutations.cs
+++ Content.Server/_Reserve/EntityEffects/Effects/PlantMetabolism/PlantClearMutations.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 Kutosss <162154227+Kutosss@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 sa1nt7331 <202271576+sa1nt7331@users.noreply.github.com>
 //
