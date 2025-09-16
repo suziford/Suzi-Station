8852eb34fce9c60409292fb88180c32b1e24ebd8
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-09-16 12:38:34 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Bingle/BinglePitSystem.cs
+++ Content.Goobstation.Server/Bingle/BinglePitSystem.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2025 Kutosss <162154227+Kutosss@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 NazrinNya <137837419+NazrinNya@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
