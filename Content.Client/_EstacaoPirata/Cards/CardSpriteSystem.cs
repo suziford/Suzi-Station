fc0dc33919bf157fa6067828a730c916dba66691
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-11-04 17:41:11 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_EstacaoPirata/Cards/CardSpriteSystem.cs
+++ Content.Client/_EstacaoPirata/Cards/CardSpriteSystem.cs
@@ -2,6 +2,9 @@
 // SPDX-FileCopyrightText: 2024 RadsammyT <32146976+RadsammyT@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Kutosss <162154227+Kutosss@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
